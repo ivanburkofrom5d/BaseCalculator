f5fced74346a3bff50a13d3776e7c6000c90ca17
ванёк <@WIN-5SBTHR8KSDR>
2024-02-25 14:27:10 +0300

    Добавьте файлы проекта.

---
+++ BaseCalculator/Calculator.cs
@@ -0,0 +1,47 @@
+﻿class Calculator{
+
+    public int Calc(string znak, int number1, int number2) {
+        int result = 0;
+
+        if(znak == "+")
+        {
+
+
+            result = number1 + number2;
+        }
+        else if(znak == "-")
+        {
+            result = number1 - number2;
+
+
+
+
+
+        }
+        
+        else if(znak == "/")
+        {
+            result = number1 / number2;
+
+
+        }
+    
+        else if(znak == "*")
+        {
+            result = number1 * number2;
+
+
+        }
+        else
+        {
+
+            Console.WriteLine("Неверная Операция");
+
+        }
+
+        return result;
+
+    }
+
+
+}
---
+++ BaseCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿Calculator calculator = new Calculator();
+
+int res1 = calculator.Calc("-", 2, 1);
+int res2 = calculator.Calc("+", 2, 1);
+int res3 = calculator.Calc("/", 4, 1);
+int res4 = calculator.Calc("*", 2, 1);
+
+int res5 = calculator.Calc("3", 1, 2);
+
+
+
+Console.WriteLine(res1);
+Console.WriteLine(res2);
+Console.WriteLine(res3);
+Console.WriteLine(res4);
+Console.WriteLine(res5);
